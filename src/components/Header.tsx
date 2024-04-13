b87647d766343af6ed8f1afbd07dbefc0bf2652f
Sofia Berlin <sofiamberlin@gmail.com>
2024-04-12 18:58:15 -0700

    Create Header component, logo image, and styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../images/swlogo.png'
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className="Header bg-dark-color">
+      <div className="wrapper p-2 flex flex-col md:flex-row gap-4 justify-between items-center">
+        <div className="logo">
+        <img src={logo} className="logo object-contain max-w-52" alt="Star Wars Character App logo" />
+        </div>
+        <div className="title grow mx-16">
+          <h1>Star Wars Character App</h1>
+        </div>
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
