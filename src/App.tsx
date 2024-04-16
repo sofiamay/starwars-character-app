f060d9ea30be50739a698371af928c01cb5574c2
Sofia Berlin <sofiamberlin@gmail.com>
2024-04-15 18:19:42 -0700

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,10 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Header />
+      <div className="app-description">
+
+      </div>
     </div>
   );
 }
