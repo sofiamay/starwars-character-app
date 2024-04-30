ffea3d03337b0840d82ea8e944f50b49574a5d1c
Sofia Berlin <sofiamberlin@gmail.com>
2024-04-30 01:11:10 -0700

    Header: Add image padding and make font size smaller

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ function Header() {
   return (
     <header className="Header bg-dark-color">
       <div className="wrapper p-2 flex flex-col md:flex-row gap-4 justify-between items-center">
-        <div className="logo">
-        <img src={logo} className="logo object-contain max-w-52" alt="Star Wars Character App logo" />
+        <div className="logo ml-4">
+        <img src={logo} className="logo object-contain max-w-60" alt="Star Wars Character App logo" />
         </div>
         <div className="title grow mx-16">
           <h1 className="heading text-center accent">Star Wars</h1>
